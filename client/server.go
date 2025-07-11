dcf6f57fc96e5312617be4b1cc01d970c8740b95
Tejasv Sharma <tejasvonly@gmail.com>
2025-07-11 12:10:52 +0530

    feat: fee api

--- client/server.go
+++ client/server.go
@@ -5,6 +5,7 @@ import "github.com/Suited-Entertainment/xrpl-go/model/client/server"
 type Server interface {
 	GetServerState(req *server.ServerStateRequest) (*server.ServerStateResponse, XRPLResponse, error)
 	GetServerInfo(req *server.ServerInfoRequest) (*server.ServerInfoResponse, XRPLResponse, error)
+	GetFee(req *server.FeeRequest) (*server.FeeResponse, XRPLResponse, error)
 }
 
 type serverImpl struct {
@@ -24,4 +25,8 @@ func (s *serverImpl) GetServerInfo(req *server.ServerInfoRequest) (*server.Serve
 
 func (s *serverImpl) GetServerState(req *server.ServerStateRequest) (*server.ServerStateResponse, XRPLResponse, error) {
 	return defaultCall[*server.ServerStateRequest, server.ServerStateResponse](s, req)
+}
+
+func (s *serverImpl) GetFee(req *server.FeeRequest) (*server.FeeResponse, XRPLResponse, error) {
+	return defaultCall[*server.FeeRequest, server.FeeResponse](s, req)
 }
